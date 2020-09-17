b8b5e2c57463aae4829fc1bd71eee6efcac6c2e2
Max Rosenberg <mjrosenberg99@gmail.com>
2020-09-17 14:17:23 -0400

    Create preorderTraversal.js

---
+++ preorderTraversal.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let ans = [];
+    let traverse = (node) => {
+        if (node === null){
+            return;
+        }
+        ans.push(node.val);
+        traverse(node.left);
+        traverse(node.right);
+    }
+    traverse(root);
+    return ans;
+};
